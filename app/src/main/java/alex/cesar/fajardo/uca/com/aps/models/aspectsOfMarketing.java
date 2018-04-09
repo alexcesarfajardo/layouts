1a78427812b19082f16941fa6af7b52a712a2d34
alexcesarfajardo <alex.fajardo27@yahoo.com>
2018-04-09 01:26:57 -0600

    layouts2

---
+++ app/src/main/java/alex/cesar/fajardo/uca/com/aps/models/aspectsOfMarketing.java
@@ -0,0 +1,136 @@
+package alex.cesar.fajardo.uca.com.aps.models;
+
+public class aspectsOfMarketing {
+
+    //aspectsofmarketing1
+    private String acopio;
+    private String intermediario;
+    private String cooperativa;
+    private String queseros;
+
+    //aspects ofmarketing2
+    private String acopioLocal;
+    private String subastas;
+    private String matadero;
+    private String corrales;
+    private String repastador;
+
+    //aspects of marketing 3
+    private float precioPromLeche;
+    private float precioPromMenor250;
+    private float precioProm251a300;
+    private float precioProm301a380;
+    private float precioPromMayor380;
+
+    public String getAcopio() {
+        return acopio;
+    }
+
+    public void setAcopio(String acopio) {
+        this.acopio = acopio;
+    }
+
+    public String getIntermediario() {
+        return intermediario;
+    }
+
+    public void setIntermediario(String intermediario) {
+        this.intermediario = intermediario;
+    }
+
+    public String getCooperativa() {
+        return cooperativa;
+    }
+
+    public void setCooperativa(String cooperativa) {
+        this.cooperativa = cooperativa;
+    }
+
+    public String getQueseros() {
+        return queseros;
+    }
+
+    public void setQueseros(String queseros) {
+        this.queseros = queseros;
+    }
+
+    public String getAcopioLocal() {
+        return acopioLocal;
+    }
+
+    public void setAcopioLocal(String acopioLocal) {
+        this.acopioLocal = acopioLocal;
+    }
+
+    public String getSubastas() {
+        return subastas;
+    }
+
+    public void setSubastas(String subastas) {
+        this.subastas = subastas;
+    }
+
+    public String getMatadero() {
+        return matadero;
+    }
+
+    public void setMatadero(String matadero) {
+        this.matadero = matadero;
+    }
+
+    public String getCorrales() {
+        return corrales;
+    }
+
+    public void setCorrales(String corrales) {
+        this.corrales = corrales;
+    }
+
+    public String getRepastador() {
+        return repastador;
+    }
+
+    public void setRepastador(String repastador) {
+        this.repastador = repastador;
+    }
+
+    public float getPrecioPromLeche() {
+        return precioPromLeche;
+    }
+
+    public void setPrecioPromLeche(float precioPromLeche) {
+        this.precioPromLeche = precioPromLeche;
+    }
+
+    public float getPrecioPromMenor250() {
+        return precioPromMenor250;
+    }
+
+    public void setPrecioPromMenor250(float precioPromMenor250) {
+        this.precioPromMenor250 = precioPromMenor250;
+    }
+
+    public float getPrecioProm251a300() {
+        return precioProm251a300;
+    }
+
+    public void setPrecioProm251a300(float precioProm251a300) {
+        this.precioProm251a300 = precioProm251a300;
+    }
+
+    public float getPrecioProm301a380() {
+        return precioProm301a380;
+    }
+
+    public void setPrecioProm301a380(float precioProm301a380) {
+        this.precioProm301a380 = precioProm301a380;
+    }
+
+    public float getPrecioPromMayor380() {
+        return precioPromMayor380;
+    }
+
+    public void setPrecioPromMayor380(float precioPromMayor380) {
+        this.precioPromMayor380 = precioPromMayor380;
+    }
+}
